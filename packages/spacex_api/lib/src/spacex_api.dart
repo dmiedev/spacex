8e74336cad7a9e85337c774ac267764fbc329b45
Dmitry Egorov <egorovdima60@gmail.com>
2022-07-06 22:11:58 +0200

    feat: generate API package base using very_good_cli

---
+++ packages/spacex_api/lib/src/spacex_api.dart
@@ -0,0 +1,7 @@
+/// {@template spacex_api}
+/// A Dart API client for the unofficial open-source SpaceX REST API.
+/// {@endtemplate}
+class SpacexApi {
+  /// {@macro spacex_api}
+  const SpacexApi();
+}
