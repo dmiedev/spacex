5d7dc5bc337af335153460525b168412430a03f8
Dmitry Egorov <egorovdima60@gmail.com>
2022-07-06 15:05:55 +0200

    feat: create app base using very_good_cli

---
+++ android/app/src/main/kotlin/dev/dmie/spacex/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.dmie.spacex.spacex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:spacex/app/app.dart';
+import 'package:spacex/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
