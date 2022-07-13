6e1e4d9fb7c2af1a7f654012fe078752b18e75bd
Dmitry Egorov <egorovdima60@gmail.com>
2022-07-13 14:56:04 +0200

    feat: add LaunchRepository stub

--- packages/launch_repository/lib/src/launch_repository.dart
+++ packages/launch_repository/lib/src/launch_repository.dart
@@ -1,7 +1,30 @@
-/// {@template launch_repository}
-/// A Dart package that manages the rocket launch domain.
-/// {@endtemplate}
+import 'package:spacex_api/spacex_api.dart';
+
+/// A repository that manages the rocket launch domain.
 class LaunchRepository {
-  /// {@macro launch_repository}
-  const LaunchRepository();
+  /// Creates a repository that manages the rocket launch domain.
+  LaunchRepository({
+    SpacexApiClient? spacexApiClient,
+  }) : _spacexApiClient = spacexApiClient ?? SpacexApiClient();
+
+  final SpacexApiClient _spacexApiClient;
+
+  /// Fetches SpaceX rocket launches that meet the specified parameters.
+  List<Launch> fetchLaunches({
+    required int amount,
+    List<FilteringOption>? filters,
+    SortingOption? sorting,
+    String? searchedText,
+    required int listNumber,
+  }) {
+    return [];
+  }
+}
+
+class FilteringOption {}
+
+class SortingOption {}
+
+enum LaunchFeature {
+  flightNumber,
 }
