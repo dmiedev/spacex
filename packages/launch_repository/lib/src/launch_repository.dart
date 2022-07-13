d91ace574f2699b0b07fa452331c7e7c243ffdc8
Dmitry Egorov <egorovdima60@gmail.com>
2022-07-13 02:20:51 +0200

    feat: generate launch repository base with very_good_cli

---
+++ packages/launch_repository/lib/src/launch_repository.dart
@@ -0,0 +1,7 @@
+/// {@template launch_repository}
+/// A Dart package that manages the rocket launch domain.
+/// {@endtemplate}
+class LaunchRepository {
+  /// {@macro launch_repository}
+  const LaunchRepository();
+}
