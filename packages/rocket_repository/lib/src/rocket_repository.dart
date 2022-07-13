253d53b6710dfbf0bc9e93edf5973e473e2159a8
Dmitry Egorov <egorovdima60@gmail.com>
2022-07-13 22:07:07 +0200

    feat: generate rocket repository package base using very_good_cli

---
+++ packages/rocket_repository/lib/src/rocket_repository.dart
@@ -0,0 +1,7 @@
+/// {@template rocket_repository}
+/// A Dart package that manages the rocket domain.
+/// {@endtemplate}
+class RocketRepository {
+  /// {@macro rocket_repository}
+  const RocketRepository();
+}
