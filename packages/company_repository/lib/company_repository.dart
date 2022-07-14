a298ca876d14cfc3d45643ba7b71967235f252d5
Dmitry Egorov <egorovdima60@gmail.com>
2022-07-14 16:04:23 +0200

    feat: add CompanyRepository

--- packages/company_repository/lib/company_repository.dart
+++ packages/company_repository/lib/company_repository.dart
@@ -1,3 +1,4 @@
 library company_repository;
 
 export 'src/company_repository.dart';
+export 'src/exceptions.dart';
--- packages/company_repository/lib/src/company_repository.dart
+++ packages/company_repository/lib/src/company_repository.dart
@@ -1,7 +1,23 @@
-/// {@template company_repository}
-/// A Dart package to manage the company domain.
-/// {@endtemplate}
+import 'package:company_repository/src/exceptions.dart';
+import 'package:spacex_api/spacex_api.dart';
+
+/// A repository that manages the company domain.
 class CompanyRepository {
-  /// {@macro company_repository}
-  const CompanyRepository();
+  /// Creates a repository that manages the company domain.
+  CompanyRepository({
+    SpacexApiClient? spacexApiClient,
+  }) : _spacexApiClient = spacexApiClient ?? SpacexApiClient();
+
+  final SpacexApiClient _spacexApiClient;
+
+  /// Fetches SpaceX company information.
+  ///
+  /// Throws [CompanyFetchingException] if fetching fails.
+  Future<Company> fetchCompany() {
+    try {
+      return _spacexApiClient.fetchCompany();
+    } on Exception {
+      throw CompanyFetchingException();
+    }
+  }
 }
