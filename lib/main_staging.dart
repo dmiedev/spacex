94174cefbc8181d63a40e4cd8c206540d9f56739
Dmitry Egorov <egorovdima60@gmail.com>
2022-07-31 18:27:25 +0200

    feat: initialize LocalSettingsApi in main()

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:launch_repository/launch_repository.dart';
+import 'package:local_settings_api/local_settings_api.dart';
 import 'package:rocket_repository/rocket_repository.dart';
 import 'package:spacex/app/app.dart';
 import 'package:spacex/bootstrap.dart';
@@ -7,7 +8,11 @@ import 'package:spacex/bootstrap.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final launchRepository = LaunchRepository();
+  final localSettingApi = LocalSettingsApi(
+    sharedPreferences: await SharedPreferences.getInstance(),
+  );
+
+  final launchRepository = LaunchRepository(localSettingsApi: localSettingApi);
   final rocketRepository = RocketRepository();
 
   await bootstrap(
