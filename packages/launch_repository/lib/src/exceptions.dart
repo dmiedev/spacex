ba3236ef1c16a6a4ec272bf179c2b78094ce298b
Dmitry Egorov <egorovdima60@gmail.com>
2022-07-31 21:06:57 +0200

    docs: add missing docs

--- packages/launch_repository/lib/src/exceptions.dart
+++ packages/launch_repository/lib/src/exceptions.dart
@@ -5,6 +5,8 @@ abstract class _LaunchRepositoryException implements Exception {
 /// An exception that occurs while fetching rocket launches.
 class LaunchFetchingException extends _LaunchRepositoryException {}
 
+/// An exception that occurs while loading or saving launch sorting option.
 class LaunchSortingException extends _LaunchRepositoryException {}
 
+/// An exception that occurs while loading or saving launch filtering options.
 class LaunchFilteringException extends _LaunchRepositoryException {}
--- packages/local_settings_api/lib/src/exceptions.dart
+++ packages/local_settings_api/lib/src/exceptions.dart
@@ -1,5 +1,7 @@
 abstract class _SettingRepositoryException implements Exception {}
 
+/// An exception that occurs while saving a setting.
 class SettingSaveException extends _SettingRepositoryException {}
 
+/// An exception that occurs while loafing a setting.
 class SettingLoadException extends _SettingRepositoryException {}
